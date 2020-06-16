ef6efd236fd2e3b69c202f3fcab2261fffaca0f5
Kaoru Kyuno <Kaoru.Kyuno@nearline.net>
2020-06-17 00:03:34 +0900

    Use Carthage & SwiftPM dependencies

--- TestApp/AppDelegate.swift
+++ TestApp/AppDelegate.swift
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let hoge := new Hoge()
+        // Use TestFramework Hoge class
+        let hoge = Hoge()
         hoge.a()
         
         // Override point for customization after application launch.
