937970b8593fc00ff124c8c7a007a75ffe2e8376
Kaoru Kyuno <Kaoru.Kyuno@nearline.net>
2020-06-16 22:02:38 +0900

    Test app using Carthage

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Kaoru Kyuno on 2020/06/16.
+//  Copyright © 2020 Kaoru Kyuno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
